9d37fb85b2fc5659b6bbf481a1517fb5eec24af5
James Loesch <155027840+jamesloesch1@users.noreply.github.com>
2024-01-24 07:43:11 -0500

    Working script that stores two distinct values

--- script.js
+++ script.js
@@ -21,25 +21,78 @@ const divide = function(a, b) {
     return a / b;
 }; 
 
-let firstNumber;
-let secondNumber;
-let operator; 
-
 const operate = function(op, numA, numB) {
-    if (op === add) {
+    if (op === "add") {
         return add(numA, numB);
-    } else if (op === subtract) {
+    } else if (op === "subtract") {
         return subtract(numA, numB);
-    } else if (op === multiply) {
+    } else if (op === "multiply") {
         return multiply(numA, numB);
-    } else if (op === divide) {
+    } else if (op === "divide") {
         return divide(numA, numB);
     } else {
         return "Not a supported operation";
     };
 };
 
-console.log(operate(add, 3, 5));
+let firstNumber;
+let secondNumber;
+let operator; 
+let displayValue;
 
+const output = document.querySelector('#output');
+
+const numbers = document.querySelectorAll('.number');
+
+numbers.forEach(number => {
+    number.addEventListener('click', (e) => {
+        if (operator === '') {
+            displayValue = Number(output.textContent += e.target.textContent);
+            firstNumber = displayValue;
+            console.log(firstNumber);
+        } else {
+            displayValue = Number(output.textContent += e.target.textContent);
+            secondNumber = displayValue;
+            console.log(secondNumber);
+        };
+    }); 
+})
+
+const addButton = document.querySelector('.operator.add');
+const subtractButton = document.querySelector('.operator.subtract');
+const multiplyButton = document.querySelector('.operator.multiply');
+const divideButton = document.querySelector('.operator.divide');
+const equalsButton = document.querySelector('.operator.equals');
+const clearButton = document.querySelector('.operator.clear');
+
+
+clearButton.addEventListener('click', () => {
+    output.textContent = '';
+});
+
+addButton.addEventListener('click', () => {
+    operator = "add";
+    Number(output.textContent = '');
+});
+
+subtractButton.addEventListener('click', () => {
+    operator = "subtract";
+    Number(output.textContent = '');
+});
+
+multiplyButton.addEventListener('click', () => {
+    operator = "multiply";
+    Number(output.textContent = '');
+});
+
+divideButton.addEventListener('click', () => {
+    operator = "divide";
+    Number(output.textContent = '');
+});
+
+equalsButton.addEventListener('click', () => {
+    operate(operator, firstNumber, secondNumber);
+    console.log(operate);
+});
 
 
